df7a221ccb907a2fe0d307556489e3df98f5b36d
subhashini789 <sandunisubashini22@gmail.com>
2025-03-14 18:41:48 +0530

    Update hello1.js

--- hello1.js
+++ hello1.js
@@ -30,6 +30,27 @@ for( i=10;i>0;i--){
 }
 
 //reverse the numbers in left and right 4321-5-9876
+let str = "4321-5-9876";
+let left = "", middle = "", right = "";
+let dash = 0;
+
+for(let i = 0; i < str.length; i++){
+	if(str[i] == "-"){
+		dash++;
+	}
+	else if(dash == 0){
+		left = str[i] +left;
+	}
+	else if(dash == 1){
+		middle = str[i];
+	}
+	else{
+		right = str[i] + right;
+	}
+}
+
+let result = left + "-" + middle + "-" + right;
+console.log(result);  
 
 
 console.log(parseInt(9/2))
