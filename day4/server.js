2e4424dce0099a3db6899efe3fd7e96cdc48e900
subhashini789 <sandunisubashini22@gmail.com>
2025-03-23 12:24:43 +0530

    update day 4

---
+++ day4/server.js
@@ -0,0 +1,16 @@
+const {createServer} = require('node:http');
+
+const localhost = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('content-Type','Text/plain')
+    res.end("Hello Nodes JS");
+
+});
+
+
+server.listen(port,localhost,()=>{
+    console.log(`Running on:${localhost}:${port}`);
+});
